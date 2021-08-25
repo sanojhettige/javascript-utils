858d5e9a0978e0e5e304ec10a89f31a3e7720e54
sanoj hettige <sanojhettige@gmail.com>
2021-08-25 22:54:13 +0530

    Created byte converter

---
+++ bytesConverter.js
@@ -0,0 +1,12 @@
+// this function requires a digit value as bytes and the decimal places required by the response to return conveted bytes
+function(bytes, decimals = 2) {
+  if (bytes === 0) return '0 Bytes';
+
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+  return `${parseFloat((bytes / k ** i).toFixed(dm))}${sizes[i]}`;
+};
