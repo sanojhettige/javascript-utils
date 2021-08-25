5924763e3c65848ecf9abc89ceb2bf8881ff55cd
sanoj hettige <sanojhettige@gmail.com>
2021-08-25 22:49:24 +0530

    Function update

--- alphabaticSort.js
+++ alphabaticSort.js
@@ -1,1 +1,14 @@
+// this function compare two any value of two objects
+const compareAndSort = (objA, objB, key) => {
+  const firstObj = objA[key].toUpperCase();
+  const secondObj = objB[key].toUpperCase();
 
+  let comparison = 0;
+  if (firstObj > secondObj) {
+    comparison = 1;
+  } else if (firstObj < secondObj) {
+    comparison = -1;
+  }
+  return comparison;
+};
+export default compareAndSort;
